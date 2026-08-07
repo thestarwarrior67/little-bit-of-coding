ebc0ade9b829383b7b36b87c7748e81a7489a95f
Ash <fgdf99418@gmail.com>
2026-08-07 18:18:19 +0530

    Add files via upload

---
+++ FeedbackForm.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title("Feedback Form")
+st.write("Please fill out the form below to provide your feedback.")
+
+with st.form("form_id"):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        name = st.text_input("Name")
+        email = st.text_input("Email")
+
+    with col2:
+        phone_number = st.number_input("Phone Number", max_characters=10)
+        Gender = st.radio("Select your Gender", ("Male", "Female", "Other"))
+        st.write(f"You Selected: {Gender}")
+
+    feedback = st.text_area("Your Feedback", max_chars=200)
+    st.caption(f"{len(feedback)}/200 characters used")
+
+    submitted = st.form_submit_button("Submit Feedback")
+    if submitted:
+        st.success(f"✅Thank you {name} for your feedback!")
